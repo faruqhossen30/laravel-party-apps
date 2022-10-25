0ea789315068eda612481b2cdff690823e7776aa
faruqhossen30 <faruqhossen30@gmail.com>
2022-10-25 10:06:56 +0600

    location package added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminlistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::prefix('admin')->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function(){
+        Route::get('/', [AdminlistController::class, 'adminList']);
+    });
 });
